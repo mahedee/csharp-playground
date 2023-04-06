ed5c08ee3c450a6813baec9604522007d344c53a
mahedee <mahedee.hasan@gmail.com>
2023-04-06 17:01:21 -0300

    initial extension demo

---
+++ src/extension-method/ExtensionMethodDemo/ExtensionMethodDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+public static class StringExtensions
+{
+    public static string Reverse(this string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+
+        return new string(charArray);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        string orignialString = "Hello world";
+        Console.WriteLine(orignialString);
+
+        string reverseString = orignialString.Reverse();
+        Console.WriteLine(reverseString);
+    }
+}
