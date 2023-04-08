2c558316a34eda11c81533c844635e20f35b43c6
mahedee <mahedee.hasan@gmail.com>
2023-04-08 01:36:38 -0300

    ....

---
+++ courses/pluralsight-csharp5-Events-Delegates-and-Lambdas/src/module02/DelegatesDemo/MulticastDelegatesDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+
+namespace DelegatesDemo
+{
+    // Declare a delegate with delegate keyword
+    public delegate void WorkPerformedDelegate(int hours, WorkType workType);
+    public enum WorkType
+    {
+        Meeting,
+        Study,
+        Playing
+    }
+
+    internal class Program
+    {
+        // Create Handler 1 method with exact signature of delegate
+        public static void WorkPerformedHandler1(int  hour, WorkType workType)
+        {
+            Console.WriteLine("Work performed handler 1 with hour log: " + hour);
+        }
+
+        // Create Handler 2 method with exact signature of delegate
+        public static void WorkPerformedHandler2(int hour, WorkType workType)
+        {
+            Console.WriteLine("Work performed handler 2 with hour log: " + hour);
+        }
+
+        // Create Handler 3 method with exact signature of delegate
+        public static void WorkPerformedHandler3(int hour, WorkType workType)
+        {
+            Console.WriteLine("Work performed handler 3 with hour log: " + hour);
+        }
+
+        static void Main(string[] args)
+        {
+            // Instastciate delegate and bind handler method
+            WorkPerformedDelegate workPerformedDelegate1 = new WorkPerformedDelegate(WorkPerformedHandler1);
+ 
+
+            // Instastciate delegate and bind handler method
+            WorkPerformedDelegate workPerformedDelegate2 = new WorkPerformedDelegate(WorkPerformedHandler2);
+
+            // Instastciate delegate and bind handler method
+            WorkPerformedDelegate workPerformedDelegate3 = new WorkPerformedDelegate(WorkPerformedHandler3);
+
+
+            // Concatenate delegates for multicast
+            workPerformedDelegate1 += workPerformedDelegate2 + workPerformedDelegate3;
+
+            // Or
+            // workPerformedDelegate1 += workPerformedDelegate2;
+            // workPerformedDelegate1 += workPerformedDelegate3;
+
+            // Now when you invoke one delegates all 3 registered handler will be called
+            workPerformedDelegate1(4, WorkType.Study);
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+/* Output:
+ * 
+
+Work performed handler 1 with hour log: 4
+Work performed handler 2 with hour log: 4
+Work performed handler 3 with hour log: 4
+
+ * 
+ */
