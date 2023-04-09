8cf389473a63d341050346033bc335da1335d38b
mahedee <mahedee.hasan@gmail.com>
2023-04-08 22:28:18 -0300

    ....

---
+++ courses/pluralsight-csharp5-Events-Delegates-and-Lambdas/src/module02/DelegatesDemo/DelegateInference/Program.cs
@@ -0,0 +1,64 @@
+﻿using CustomEventArgs;
+using System;
+using System.ComponentModel;
+
+namespace EventsDemo
+{
+    public enum WorkType
+    {
+        Meeting,
+        Study,
+        Playing
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+
+            // The += operator is used to attach an event to an event handler
+            //worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
+            // Use delegate inference
+            worker.WorkPerformed += Worker_WorkPerformed;
+
+            // instanciate and bind with event's invocation list
+            //worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
+            // Use delegate inference
+            worker.WorkCompleted += Worker_WorkCompleted;
+
+            // detach event handler from an event
+            worker.WorkCompleted -= Worker_WorkCompleted;
+
+            // Raise events
+            worker.DoWork(3, WorkType.Playing);
+
+
+            Console.ReadKey();
+        }
+
+        // Create Handler method with exact signature of delegate
+        private static void Worker_WorkCompleted(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Worker is done");
+        }
+
+        // Create Handler method with exact signature of delegate
+
+        private static void Worker_WorkPerformed(object? sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+        }
+    }
+}
+
+/* Output:
+ * 
+
+Hours worked: 1 Playing
+Hours worked: 2 Playing
+Hours worked: 3 Playing
+
+ * 
+ */
---
+++ courses/pluralsight-csharp5-Events-Delegates-and-Lambdas/src/module02/DelegatesDemo/DelegateInference/WorkPerformedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using EventsDemo;
+using System;
+
+namespace CustomEventArgs
+{
+    public class WorkPerformedEventArgs : EventArgs
+    {
+        public WorkPerformedEventArgs(int hours, WorkType workType)
+        {
+            Hours = hours;
+            WorkType = workType;
+        }
+
+        public int Hours { get; set; }
+        public WorkType WorkType { get; set; }
+    }
+}
