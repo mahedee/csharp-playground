cb13e373ba385592fda113a4e448276f8b34b558
mahedee <mahedee.hasan@gmail.com>
2023-04-09 04:29:18 -0300

    ....

--- courses/pluralsight-csharp5-Events-Delegates-and-Lambdas/docs/readme.txt
+++ courses/pluralsight-csharp5-Events-Delegates-and-Lambdas/docs/readme.txt
@@ -1,5 +1,5 @@
 Course URL: https://app.pluralsight.com/library/courses/csharp-events-delegates/table-of-contents
-Completed up to: 1.3: Introduction
+Completed up to: 5.2
 
 Required tools
 - ILSpy : Decompile C# code - https://apps.microsoft.com/store/detail/ilspy/9MXFBKFVSQ13?hl=en-ca&gl=ca&rtc=1
---
+++ courses/pluralsight-csharp5-Events-Delegates-and-Lambdas/src/CommunicatingBetweenControls/CommunicatingBetweenControls/Mediator.cs
@@ -0,0 +1,34 @@
+﻿using CommunicatingBetweenControls.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicatingBetweenControls
+{
+    public class Mediator
+    {
+        //static member
+
+        public static readonly Mediator _instance = new Mediator();
+        private Mediator() { }
+
+        public static Mediator GetInstance() { return _instance; }
+
+        // Instance functionality
+        public event EventHandler<JobChangedEventArgs> JobChanged;
+
+        public void OnJobChanged(object sender, Job job)
+        {
+            var jobChangedDelegate = JobChanged as EventHandler<JobChangedEventArgs>;
+
+            if (jobChangedDelegate != null)
+            {
+                jobChangedDelegate(sender, new JobChangedEventArgs() { Job = job });
+            }
+        }
+
+
+    }
+}
