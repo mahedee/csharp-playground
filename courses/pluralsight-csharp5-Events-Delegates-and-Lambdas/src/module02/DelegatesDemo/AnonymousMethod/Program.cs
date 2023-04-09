552c2525d5064f68833b37e0b1a2e951ee7a29ae
mahedee <mahedee.hasan@gmail.com>
2023-04-08 23:13:02 -0300

    ....

---
+++ courses/pluralsight-csharp5-Events-Delegates-and-Lambdas/src/module02/DelegatesDemo/AnonymousMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿using CustomEventArgs;
+using System;
+using System.ComponentModel;
+
+namespace EventsDemo
+{
+    public enum WorkType
+    {
+        Meeting,
+        Study,
+        Playing
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+
+
+            // Anonymous methods allow event handler code to be hooked directly to an event
+            // Anonymous methods are defined using the delegate keyword
+            
+            worker.WorkPerformed += delegate (object? sender, WorkPerformedEventArgs e)
+            {
+                Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+            };
+
+
+            worker.WorkCompleted += delegate (object? sender, EventArgs e) 
+            {
+                Console.WriteLine("Worker is done");
+            };
+
+            // Raise events
+            worker.DoWork(3, WorkType.Playing);
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+/* Output:
+ * 
+
+Hours worked: 1 Playing
+Hours worked: 2 Playing
+Hours worked: 3 Playing
+Worker is done
+
+ * 
+ */
---
+++ courses/pluralsight-csharp5-Events-Delegates-and-Lambdas/src/module02/DelegatesDemo/AnonymousMethod/Worker.cs
@@ -0,0 +1,67 @@
+﻿using CustomEventArgs;
+using System;
+
+namespace EventsDemo
+{
+    // Declare a delegate with delegate keyword. You can use like below or change using generic
+    // public delegate void WorkPerformedHandler(object sender, WorkPerformedEventArgs e);
+
+    public class Worker
+    {
+        //public event WorkPerformedHandler WorkPerformed;
+
+        // Instead of using the previous statement we can use the following code using generics
+        // EventHandler<T> provides a simple way to create a custom delegate for an event
+        public EventHandler<WorkPerformedEventArgs> WorkPerformed;
+        
+        // EventHandler is built in delegates of .net 
+        public event EventHandler WorkCompleted;
+
+
+        // Raise events
+        public void DoWork(int hours, WorkType workType)
+        {
+            for(int i = 0; i < hours; i++)
+            {
+                // Sleep for 1 second to see it's working. it's not mandatory
+                System.Threading.Thread.Sleep(1000);
+                OnWorkPerformed(i + 1, workType);
+            }
+
+            OnWorkCompleted();
+        }
+
+        // kept protected virtual means any one can inherite and override the method. You can keep it private
+        // On is used in the method name as convention not rule
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+     
+            //var del = WorkPerformed as WorkPerformedHandler;
+
+            var del = WorkPerformed as EventHandler<WorkPerformedEventArgs>;
+
+            if (del != null)
+            {
+                //del(hours, workType);
+                del(this, new WorkPerformedEventArgs(hours, workType));
+            }
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            // You can call either way. 
+
+            //if(WorkCompleted != null)
+            //{
+            //    WorkCompleted(hours, workType);
+            //}
+
+            var del = WorkCompleted as EventHandler;
+            if (del != null)
+            {
+                // here this class is sender or raise the event
+                del(this, EventArgs.Empty);
+            }
+        }
+    }
+}
