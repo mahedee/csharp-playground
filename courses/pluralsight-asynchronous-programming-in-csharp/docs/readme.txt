f29a880902bfbbd86c21d21160c0a5703610a35d
mahedee <mahedee.hasan@gmail.com>
2023-05-11 12:29:01 -0300

    ....

--- courses/pluralsight-asynchronous-programming-in-csharp/docs/readme.txt
+++ courses/pluralsight-asynchronous-programming-in-csharp/docs/readme.txt
@@ -1,6 +1,6 @@
 Course Name: Asynchronous Programming in C# 10 by Filip Ekberg
 Course URL: https://app.pluralsight.com/library/courses/c-sharp-10-asynchronous-programming/table-of-contents
 
-Completed up to: 2.8 - Best Practices
+Completed up to: 3.3 - Obtaining the Result of a Task
 
 Notes:
---
+++ courses/pluralsight-asynchronous-programming-in-csharp/src/PracticeProjects/03/demos/Windows/MyPractice/StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using Newtonsoft.Json;
+using StockAnalyzer.Core;
+using StockAnalyzer.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace StockAnalyzer.Windows;
+
+public partial class MainWindow : Window
+{
+    private static string API_URL = "https://ps-async.fekberg.com/api/stocks";
+    private Stopwatch stopwatch = new Stopwatch();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+
+
+    private async void Search_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            BeforeLoadingStockData();
+
+            await Task.Run(() =>
+            {
+                var lines = File.ReadAllLines("StockPrices_Small.csv");
+
+                var data = new List<StockPrice>();
+
+                foreach (var line in lines.Skip(1))
+                {
+                    var price = StockPrice.FromCSV(line);
+                    data.Add(price);
+                }
+
+                Dispatcher.Invoke(() => {
+                    Stocks.ItemsSource = data.Where(sp => sp.Identifier == StockIdentifier.Text);
+                });
+                
+                //await GetStocks();
+            });
+        }
+        catch (Exception ex)
+        {
+            Notes.Text = ex.Message;
+        }
+        finally
+        {
+            AfterLoadingStockData();
+        }
+    }
+
+    private async Task GetStocks()
+    {
+        try
+        {
+            var store = new DataStore();
+
+            var responseTask = store.GetStockPrices(StockIdentifier.Text);
+
+            Stocks.ItemsSource = await responseTask;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    private void BeforeLoadingStockData()
+    {
+        stopwatch.Restart();
+        StockProgress.Visibility = Visibility.Visible;
+        StockProgress.IsIndeterminate = true;
+    }
+
+    private void AfterLoadingStockData()
+    {
+        StocksStatus.Text = $"Loaded stocks for {StockIdentifier.Text} in {stopwatch.ElapsedMilliseconds}ms";
+        StockProgress.Visibility = Visibility.Hidden;
+    }
+
+    private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo { FileName = e.Uri.AbsoluteUri, UseShellExecute = true });
+
+        e.Handled = true;
+    }
+
+    private void Close_OnClick(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}
